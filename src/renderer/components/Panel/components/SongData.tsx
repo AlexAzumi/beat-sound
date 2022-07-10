5ec2c2053eaeefa1e7c7a23f78c03f15a3b6be51
Alejandro Suárez <alejandro-hdez115@outlook.com>
2022-07-09 22:53:28 -0500

    Add a "no cover" image

--- src/renderer/components/Panel/components/SongData.tsx
+++ src/renderer/components/Panel/components/SongData.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react';
 import { Image } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCompactDisc } from '@fortawesome/free-solid-svg-icons';
 
 interface SongDataProps {
   /**
@@ -19,7 +21,13 @@ interface SongDataProps {
 const SongData: FC<SongDataProps> = ({ artist, name, imageSrc }) => {
   return (
     <>
-      <Image height={70} src={imageSrc} width={70} />
+      {!imageSrc ? (
+        <div className="no-cover d-flex justify-content-center align-items-center">
+          <FontAwesomeIcon icon={faCompactDisc} size="3x" />
+        </div>
+      ) : (
+        <Image height={70} src={imageSrc} width={70} />
+      )}
       <div className="d-flex flex-column px-2 py-1 justify-content-center text-truncate">
         <p className="m-0 fw-bold">{name}</p>
         <p className="m-0">{artist}</p>
